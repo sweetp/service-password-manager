c82928fca31eeb6a6add436c40c8708ef7a298a1
Stefan Gojan <vollekannehoschi@gmail.com>
2012-12-26 12:07:55 +0100

    init

---
+++ password-manager.js
@@ -0,0 +1,43 @@
+var sweetp = require('./lib/sweetp');
+
+service = {
+	getConfig:function() {
+		var config = {
+			'password/manager/get':{
+				method:'get',
+				params: {
+					name: 'one'
+				}
+			}
+		};
+		return JSON.stringify(config);
+	},
+
+	get:function(params) {
+		return "test " + params.name;
+	}
+};
+
+sweetp.start(service);
+
+    // spawn command line command
+/*
+ *    var ls    = spawn('git', ['status']);
+ *
+ *    ls.stdout.on('data', function (data) {
+ *        sys.print(data);
+ *        con.write(data);
+ *    });
+ *
+ *    ls.stderr.on('data', function (data) {
+ *        sys.print('stderr: ' + data);
+ *        con.write('stderr: ' + data);
+ *    });
+ *
+ *    ls.on('exit', function (code) {
+ *        console.log('child process exited with code ' + code);
+ *        con.write('child process exited with code ' + code);
+ *        con.destroy();
+ *    });
+ */
+
