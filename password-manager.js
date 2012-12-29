f29830cd6deb5b1e2b9f022a5273fe8aa70e1bc0
Stefan Gojan <vollekannehoschi@gmail.com>
2012-12-29 19:34:51 +0100

    add package file with dependencies

--- password-manager.js
+++ password-manager.js
@@ -1,8 +1,16 @@
 var sweetp = require('./lib/sweetp');
 var http = require('http');
+var Crypto = require('ezcrypto').Crypto;
+var bcrypt = require('bcrypt');
 
 var service, methods, client;
 
+// private helper functions for service methods
+function foo(name) {
+	return "foobar" + name;
+}
+
+// public service functions
 service = {
 	get:{
 		options: {
@@ -10,18 +18,58 @@ service = {
 				name: 'one'
 			}
 		},
-		target:'/password/manager/get',
 		fn:function(params) {
+			// get user and password for 'name'
+			return foo(params.name);
+		}
+	},
+
+	set:{
+		options: {
+			params: {
+				name: 'one',
+				user: 'one',
+				password: 'one'
+			}
+		},
+		fn:function(params) {
+			// set object
+			return "test " + params.name;
+		}
+	},
+
+	authenticate:{
+		options: {
+			params: {
+				url: 'url'
+			}
+		},
+		fn:function(params) {
+			// call service to retrieve master password by user and decrypt password safe
+			return "test " + params.name;
+		}
+	},
+
+	createPasswordStore:{
+		options: {
+			params: {
+				url: 'url'
+			}
+		},
+		fn:function(params) {
+			// create master password for this project
 			return "test " + params.name;
 		}
 	}
 };
 
-methods = sweetp.createMethods(service);
+// create service methods and start sweetp service (client)
+methods = sweetp.createMethods(service, '/password/manager/');
 client = sweetp.start(methods);
 
+// only used for development
 process.once('SIGUSR2', function() {
-	console.log('Got a SIGUSR2');
+	console.log('restarting...');
 	client.closeSocket();
 	http.get({
 		host:'localhost',
