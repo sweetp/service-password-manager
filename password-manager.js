655b9fa8962dcefcfa9771b7cd24c680c67a0e60
Stefan Gojan <vollekannehoschi@gmail.com>
2012-12-30 16:17:37 +0100

    add create service method

--- password-manager.js
+++ password-manager.js
@@ -2,16 +2,75 @@ var sweetp = require('./lib/sweetp');
 var http = require('http');
 var Crypto = require('ezcrypto').Crypto;
 var bcrypt = require('bcrypt');
+var fs = require('fs');
+var path = require('path');
 
-var service, methods, client;
+var
+
+/**
+ * {Object} service methods with sweetp meta data
+ * @private
+ */
+service,
+methods, client, salt, passwords, master;
+
+// init
+salt = bcrypt.genSaltSync(10);
+master = {};
+passwords = {};
 
 // private helper functions for service methods
-function foo(name) {
-	return "foobar" + name;
+
+function getMasterPasswordFromUser(url) {
+	// TODO get it with service call
+	return "foobar";
 }
+
+function getDataPath(config) {
+	return path.join(config.dir, ".sweetp", "passwordSafe.json");
+}
+
+function getDataFromFile(dataPath, callback) {
+	fs.exists(dataPath, function (exists) {
+		if (!exists) {
+			throw new Error("Password file doesn't exist, call 'createStore' method to init password store in .sweetp/ directory of this project.");
+		}
+		fs.readFile(dataPath, function (err, fileData) {
+			if (err) {
+				throw new Error("Error occured during password file read: " + err);
+			}
+			data = JSON.parse( fileData.toString() );
+			if (!data.master) {
+				throw new Error("Master password hash not found in file " + dataPath);
+			}
+			masterHash = data.master;
+			// TODO finish
+			callback();
+		});
+	});
+}
+
 
 // public service functions
 service = {
+	createSafe:{
+		options: {
+			params: {
+				url: 'url',
+				config: 'projectConfig'
+			}
+		},
+		fn:function(params) {
+			var masterHash, masterPassword;
+			master[params.config.name] = masterPassword = getMasterPasswordFromUser(params.url);
+			masterHash = bcrypt.hashSync(masterPassword, salt);
+			dataPath = getDataPath(params.config);
+			passwords[params.config.name] = {};
+			fs.writeFileSync(dataPath, JSON.stringify({master:masterHash, passwords:{}}), 'utf-8');
+			return "Password safe created successfully.";
+		}
+	},
+
 	get:{
 		options: {
 			params: {
@@ -29,12 +88,12 @@ service = {
 			params: {
 				name: 'one',
 				user: 'one',
-				password: 'one'
+				password: 'one',
+				config: 'projectConfig'
 			}
 		},
 		fn:function(params) {
-			// set object
-			return "test " + params.name;
+
 		}
 	},
 
@@ -48,19 +107,8 @@ service = {
 			// call service to retrieve master password by user and decrypt password safe
 			return "test " + params.name;
 		}
-	},
-
-	createPasswordStore:{
-		options: {
-			params: {
-				url: 'url'
-			}
-		},
-		fn:function(params) {
-			// create master password for this project
-			return "test " + params.name;
-		}
 	}
+
 };
 
 // create service methods and start sweetp service (client)
